6dffcafbc9012ee128e079583d2b0cda436542f2
sheafk <sheafureyking@gmail.com>
2016-07-08 13:27:59 -0400

    Completed the User Interface for Simon Says game

---
+++ SimonSays-Game/SimonSays-Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimonSays-Game
+//
+//  Created by Shea Furey-King on 7/8/16.
+//  Copyright © 2016 Shea Furey-King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
